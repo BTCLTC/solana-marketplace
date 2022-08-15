a6406f2c6eb9ddc936b5fcb6d5c7ee442ba37f59
clayon <admin@github.xyz>
2022-08-15 13:05:40 +0800

    update

--- app/src/pages/buy.tsx
+++ app/src/pages/buy.tsx
@@ -1,7 +1,38 @@
 import { NextPage } from 'next';
+import { m } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import { useRecoilValue } from 'recoil';
 
-const Buy: NextPage = () => {
-  return <section>buy</section>;
+import BuyNFT from '../components/BuyNFT';
+import { appState } from '../stores';
+import { fade } from '../utils';
+import { loadMarketNfts } from '../solana/nft';
+import { INFT } from '../interface';
+
+
+const Sell: NextPage = () => {
+  const { provider } = useRecoilValue(appState);
+
+  const [nfts, setNfts] = useState<INFT[]>([]);
+
+  useEffect(() => {
+    if (provider) {
+      loadMarketNfts().then((data) => {
+        setNfts(data);
+      });
+    }
+  }, [provider]);
+
+  return (
+    <m.section
+      variants={fade}
+      className="container flex flex-wrap justify-between"
+    >
+      {nfts.map((item) => (
+        <BuyNFT key={item.mint} info={item} />
+      ))}
+    </m.section>
+  );
 };
 
-export default Buy;
+export default Sell;
--- app/src/solana/nft/index.ts
+++ app/src/solana/nft/index.ts
@@ -1,7 +1,9 @@
+import axios from 'axios';
 import { getParsedNftAccountsByOwner } from '@nfteyez/sol-rayz';
 import { AnchorProvider } from '@project-serum/anchor';
+import marketNfts from '../../data/list.json';
 
-export async function loadAllNFTs(provider: AnchorProvider) {
+export const loadAllNFTs = async (provider: AnchorProvider) => {
   try {
     const nfts = await getParsedNftAccountsByOwner({
       publicAddress: provider.wallet.publicKey.toBase58(),
@@ -12,4 +14,14 @@ export async function loadAllNFTs(provider: AnchorProvider) {
   } catch (error) {
     console.log(error);
   }
+}
+
+export const loadMarketNfts =async () => {
+  let marketNFTs = [];
+  for(let item of marketNfts) {
+    const res = await axios.get(`https://api.solscan.io/account?address=${item.mint}&cluster=devnet`);
+    marketNFTs.push(res?.data?.data?.metadata);
+  }
+  console.log('marketNFTs', marketNFTs);
+  return marketNFTs;
 }
