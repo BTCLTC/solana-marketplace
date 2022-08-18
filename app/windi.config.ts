3010ab2fd50f2ee63c371e9ebeca50f0ba5da11c
Clayons <admin@github.xyz>
2022-08-18 11:32:15 +0800

    update

--- app/windi.config.ts
+++ app/windi.config.ts
@@ -18,13 +18,6 @@ export default defineConfig({
   plugins: [require('daisyui')],
   // daisyUI config (optional)
   daisyui: {
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: '',
-    darkTheme: 'light',
+    themes: false,
   },
 });
